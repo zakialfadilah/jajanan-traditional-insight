560fae679e7a79c835de9568be3fa07ead3e1fe0
zakialfadilah <zakialfadilah24@gmail.com>
2025-06-06 00:03:18 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,11 @@ import numpy as np
 from PIL import Image
 import requests
 import os
-from tensorflow.keras.models import load_model
+import tensorflow as tf
 
-# URL model dari Hugging Face Hub
-# MODEL_URL = "https://huggingface.co/zakialfadilah/best_model_resnet50/resolve/main/best_model_resnet50.keras"
+# URL model dari Hugging Face
 MODEL_URL = "https://huggingface.co/zakialfadilah/model_vgg/resolve/main/model_vgg16.tflite"
 MODEL_PATH = "model_vgg16.tflite"
-# MODEL_PATH = "best_model_resnet50.keras"
 
 # Daftar kelas (label)
 CLASS_NAMES = [
@@ -22,19 +20,27 @@ CLASS_NAMES = [
 @st.cache_resource
 def download_and_load_model():
     if not os.path.exists(MODEL_PATH):
-        with st.spinner("Downloading model..."):
+        with st.spinner("Mengunduh model..."):
             response = requests.get(MODEL_URL)
             with open(MODEL_PATH, 'wb') as f:
                 f.write(response.content)
-    model = load_model(MODEL_PATH)
-    return model
+    interpreter = tf.lite.Interpreter(model_path=MODEL_PATH)
+    interpreter.allocate_tensors()
+    return interpreter
 
 # Fungsi prediksi gambar
-def predict_image(model, image):
-    image = image.resize((256, 256))  # Sesuaikan dengan input model
+def predict_image(interpreter, image):
+    image = image.resize((256, 256))
     image_array = np.array(image) / 255.0
-    image_array = np.expand_dims(image_array, axis=0)
-    prediction = model.predict(image_array)[0]  # Ambil hasil prediksi dari batch
+    image_array = np.expand_dims(image_array.astype(np.float32), axis=0)
+
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+
+    interpreter.set_tensor(input_details[0]['index'], image_array)
+    interpreter.invoke()
+
+    prediction = interpreter.get_tensor(output_details[0]['index'])[0]
     return prediction
 
 # Streamlit UI
@@ -47,10 +53,10 @@ if uploaded_file:
     st.image(image, caption="Gambar yang diunggah", use_container_width=True)
 
     # Load model
-    model = download_and_load_model()
+    interpreter = download_and_load_model()
 
     # Prediksi
-    prediction = predict_image(model, image)
+    prediction = predict_image(interpreter, image)
     predicted_index = np.argmax(prediction)
     predicted_label = CLASS_NAMES[predicted_index]
     confidence = prediction[predicted_index] * 100
@@ -59,7 +65,7 @@ if uploaded_file:
     st.write(f"Label: **{predicted_label}**")
     st.write(f"Akurasi: **{confidence:.2f}%**")
 
-    # Optional: tampilkan semua probabilitas
+    # Detail probabilitas semua kelas
     st.subheader("Detail Probabilitas:")
     for i, class_name in enumerate(CLASS_NAMES):
         st.write(f"{class_name}: {prediction[i]*100:.2f}%")
